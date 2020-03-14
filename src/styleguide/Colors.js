673f37b249e3be91955eca0bb28849dc2a8e5b07
Kaizaro <kaizaro78@gmail.com>
2020-03-14 20:14:27 +0300

    made fonts and colors

--- src/styleguide/Colors.js
+++ src/styleguide/Colors.js
@@ -1,3 +1,16 @@
+export const APP_COLORS = {
+    BACKGROUND_COLOR: '#FFF',
+    PRIMARY_COLOR: '#FF640B',
+    BLACK_COLOR: '#272725',
+    TEXT_MUTED: '#C2C2C2',
+    BADGE_COLORS: {
+        PINK_PURPLE: '#C7B3F9',
+        YELLOW: '#FCFB5C',
+        SPACE_BLUE: '#4437A2',
+        LIGHT_GREEN: '#35E064',
+    },
+};
+
 export const BACKGROUND_COLOR = '#FFF';
 export const PRIMARY_COLOR = '#FF640B';
 export const BLACK_COLOR = '#272725';
--- src/styleguide/Fonts.js
+++ src/styleguide/Fonts.js
@@ -1,3 +1,9 @@
+export const APP_FONTS = {
+    SOFIA_BOLD: 'Sofia Pro Bold',
+    SOFIA_PRO_EXTRA_LIGHT: 'Sofia Pro ExtraLight',
+    SOFIA_PRO_EXTRA_LIGHT_ITALIC: 'Sofia Pro ExtraLight Italic',
+};
+
 export const SOFIA_BOLD = 'Sofia Pro Bold';
 export const SOFIA_PRO_EXTRA_LIGHT = 'Sofia Pro ExtraLight';
 export const SOFIA_PRO_EXTRA_LIGHT_ITALIC = 'Sofia Pro ExtraLight Italic';
