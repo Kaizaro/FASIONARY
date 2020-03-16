9b73b0eed67a4199c5486101a80df388d9438e30
Kaizaro <kaizaro78@gmail.com>
2020-03-16 19:33:18 +0300

    made divide

---
+++ src/components/common/Divide.js
@@ -0,0 +1,9 @@
+import React, {PureComponent} from 'react';
+import {View} from 'react-native';
+import {APP_STYLES} from '../../styleguide/Styles';
+
+export default class Divide extends PureComponent {
+    render() {
+        return <View style={APP_STYLES.DIVIDE} />;
+    }
+}
